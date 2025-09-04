78a0f56898b486e4ad957f7b59c190ba9a33b2b5
Rahmehdaraghmeh <rahma.daraghmah21@gmail.com>
2025-09-04 22:30:44 +0300

    ProtectedRoute to prevent any user not register to log

--- gmp/src/App.tsx
+++ gmp/src/App.tsx
@@ -1,21 +1,41 @@
-// app.tsx
-import React from "react";
+
 import { Routes, Route } from "react-router-dom";
+import AuthPage from "./auth/AuthPage";
+import  StudentDashboard  from "./dashboards/StudentDashboard";
 import SupervisorDashboard from "./dashboards/SupervisorDashboard";
 
-import AuthPage from "./auth/AuthPage";
-import { StudentDashboard } from "./dashboards/StudentDashboard";
-
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 function App() {
   return (
- 
-      <Routes>
-             <Route path="/" element={<AuthPage/>} />
-          <Route path="/dashboard/student" element={<StudentDashboard/>} />
-          <Route path="/dashboard/supervisor" element={<SupervisorDashboard />} />
-      </Routes>
+    <Routes>
 
+      <Route path="/" element={<AuthPage />} />
+
+
+      <Route
+        path="/dashboard/student"
+        element={
+         
+            <StudentDashboard />
+         
+        }
+      >
+
+      </Route>
+      
+    
+
+      {/* مسار المشرف */}
+      <Route
+        path="/dashboard/supervisor"
+        element={
+          <ProtectedRoute allowedRoles={["supervisor"]}>
+            <SupervisorDashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
