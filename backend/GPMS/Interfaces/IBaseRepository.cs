eb8a6e887edb753cfcf402f0d0390b150d0b7492
osamafrehate <osama.frehate@gmail.com>
2025-09-10 20:37:05 +0300

    added repo layer and repo interface

---
+++ backend/GPMS/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace V1MP.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(object id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(object id);
+    }
+}
---
+++ backend/GPMS/Repositories/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using V1MP.Interfaces;
+using V1MP.Data;
+
+namespace V1MP.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+        public virtual async Task<T?> GetByIdAsync(object id) => await _dbSet.FindAsync(id);
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task<T> UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task<bool> DeleteAsync(object id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity == null) return false;
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
+
