6c94b6382cef9871b8004914ab34646efbfdaa0d
osamafrehate <osama.frehate@gmail.com>
2025-09-10 20:18:26 +0300

    added rest models

---
+++ backend/GPMS/Models/Doing.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace V1MP.Models
+{
+    public class Doing
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        // One-to-One with Team
+        [ForeignKey("Team")]
+        public long TeamId { get; set; }
+        public Team Team { get; set; }
+    }
+}
---
+++ backend/GPMS/Models/Team.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace V1MP.Models
+{
+    public class Team
+    {
+        public long TeamId { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(100)")]
+        public string TeamName { get; set; }
+
+        public bool TeamStatus { get; set; } = false;
+
+        public DateTime CreatedDate { get; set; }
+
+        // One-to-One with Project
+        [ForeignKey("Project")]
+        public string ProjectTitle { get; set; }
+        public Project Project { get; set; }
+
+        // Many-to-One with Supervisor
+        [ForeignKey("Supervisor")]
+        public long SupervisorId { get; set; }
+        public Supervisor Supervisor { get; set; }
+
+        // One-to-One with ToDo, Doing, Done
+        public Todo Todo { get; set; }
+        public Doing Doing { get; set; }
+        public Done Done { get; set; }
+
+
+        //public ICollection<Student> Students { get; set; }
+    }
+}
