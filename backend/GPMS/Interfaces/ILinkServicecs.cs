fcc2e22159089c9016e626a6d2dd246e41926b16
Malak-Malak <malaknaim2004@gmail.com>
2025-09-16 16:00:08 +0300

    added some edits

--- backend/GPMS/Interfaces/ILinkServicecs.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace GPMS.Interfaces
-{
-    public interface ILinkServicecs : ILinkRepository
-    {
-    }
-}
--- backend/GPMS/Models/Link.cs
+++ backend/GPMS/Models/Link.cs
@@ -9,11 +9,19 @@ namespace GPMS.Models
         public int Id { get; set; }
 
         [Required]
-        [Column(TypeName = "varchar(100)")]
+        [Column(TypeName = "varchar(500)")]
         public required string Url { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string Title { get; set; } = string.Empty;
 
-        [ForeignKey("KanbanTask")]
-        public int TaskId { get; set; }
-        public required KanbanTask KanbanTask { get; set; }
+        [Column(TypeName = "text")]
+        public string? Description { get; set; }
+
+        public int TeamId { get; set; }
+        public Team Team { get; set; } = null!;
+        //[ForeignKey("KanbanTask")]
+        //public int TaskId { get; set; }
+        //public required KanbanTask KanbanTask { get; set; }
     }
 }
