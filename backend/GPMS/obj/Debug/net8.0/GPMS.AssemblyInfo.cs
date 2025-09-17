fe621688982ecf9860fc0a81753c93f6aa400be7
osamafrehate <osama.frehate@gmail.com>
2025-09-17 18:38:48 +0300

    delete mapper

--- backend/GPMS/obj/Debug/net8.0/GPMS.AssemblyInfo.cs
+++ backend/GPMS/obj/Debug/net8.0/GPMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GPMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe99f17dad4d565e42a4c469fab9603b0538712")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c406a2a842792eed4eaa2d47b2664e1b69d891")]
 [assembly: System.Reflection.AssemblyProductAttribute("GPMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GPMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
